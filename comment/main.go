83a70307bb5982367a4d3a65e4bd05aa1e538a74
Afdiko Mayte <afdikomayte@gmail.com>
2024-05-05 15:00:34 +0700

    feat: comment

---
+++ comment/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+  //this line not be print to terminal because it's comment
+  fmt.Println("line above not be print cause it's comment")
+
+  /*
+  * these are multiline comment
+  * 
+  */ 
+  fmt.Println("lines above not be print they are comment")
+
+}
