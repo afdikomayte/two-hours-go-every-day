b6fc3f09086607e608a6d3ad0f205612af1ac4ff
Afdiko Mayte <afdikomayte@gmail.com>
2024-05-05 16:15:43 +0700

    feat(variable):add example declaration variable with new()

--- variable/main.go
+++ variable/main.go
@@ -22,4 +22,44 @@ func main(){
   //use fmt.Printf for print with format
   fmt.Printf("My Name is %s %s\n", firstName, lastName)
 
+  /*
+  * variable without data-type
+  * formula 
+  * <variable-name> := <value>
+  */
+
+  fullName := "Afdiko Mayte Saputra"
+  fmt.Printf("My fullname =%s\n",fullName)
+
+  /*
+  * declaration multi-variable
+  */ 
+  var fruit1,fruit2, fruit3 = "manggo","apple","strobbery"
+  // or
+  animal1, animal2, animal3 := "dog","cat","tiger"
+  
+  fmt.Println(fruit1, fruit2, fruit3)
+  fmt.Println(animal1,animal2,animal3)
+
+
+  /*
+  * variable _ (underscore) in go all variable was declaration must be use
+  * if variable not be use make variable name with _ (underscore)
+  */
+  useVaribale, _ := "use-variable","ignore-variable"
+
+  fmt.Println(useVaribale)
+
+  /*
+  * declaration variable function new()
+  * variables declared with function new() become pointer variable 
+  * formula
+  * name-variable := new(data-type)
+  */
+  
+  varPointer := new(string)
+
+  fmt.Printf("It's Variable-pointer = %x\n", varPointer)
+  fmt.Printf("It's Value Varible ponter = %s\n",*varPointer)
+
 }
