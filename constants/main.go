baeeb40379c18c427e5757564743b8f8d849307b
Afdiko Mayte <afdikomayte@gmail.com>
2024-05-06 18:02:24 +0700

    feat: constants

---
+++ constants/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+
+  /*
+  * constants varibles cannot be change once declared
+  */
+
+  const name string= "variable const"
+
+  fmt.Print("this ", name,"!\n")
+
+  /*
+  * declaration multi const varibles 
+  */ 
+  const (
+    today string = "senin"
+    sekarang //value this variable same with today 
+    isToday = true
+  ) 
+
+  fmt.Print(today,"\n",sekarang,"\n",isToday,"\n")
+}
