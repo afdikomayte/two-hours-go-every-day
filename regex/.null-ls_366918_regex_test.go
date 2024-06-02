3b8d15a23e0b927bf4fa7becdb39623c0a9362ed
Afdiko Mayte <afdikomayte@gmail.com>
2024-06-02 20:54:00 +0700

    feat: regex

---
+++ regex/.null-ls_366918_regex_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegexMatchString(t *testing.T) {
+	var text = "afdiko mayte saputra"
+	// membuat aturan regex nya
+	var regex, err = regexp.Compile(`[a-z]+`)
+
+	// melihat apakah kalimat cocok dengan aturan regex yang di buat
+	var isMatch = regex.MatchString(text)
+
+	assert.Nil(t, err, "Should be nil")
+
+	assert.Equal(t, bool(true), isMatch)
+}
